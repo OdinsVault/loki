7832f21ea8caee05fcecb410e86fdd9b212d91e2
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-09-07 23:32:09 +0530

    Test Translation Added

--- src/main/java/mapper/App.java
+++ src/main/java/mapper/App.java
@@ -1,12 +1,22 @@
 package mapper;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public final class App {
-    private App() {
+
+    private App(){
+
     }
 
+    // TODO: Init logger
+    //private static final Logger logger = LogManager.getLogger(String.valueOf(App.class));
+
     public static void main(String[] args) {
+
         MapperService mapperService = new MapperService();
         try {
             System.out.println(mapperService.map("sin", "demo_source_file_path"));
--- src/main/java/mapper/MapperService.java
+++ src/main/java/mapper/MapperService.java
@@ -1,19 +1,16 @@
 package mapper;
 
 import org.yaml.snakeyaml.Yaml;
+import org.apache.commons.io.FilenameUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Map;
 
 public class MapperService {
 
     /**
      * Map the native lang code to target lang code (English)
-     * 
+     *
      * @param mapping_id id of the mapping (english -> en, spanish -> sp)
      * @param source_file_path Source code file path
      */
@@ -30,13 +27,15 @@ public class MapperService {
         // Get mapping file
         Map<String, String> mapping = this.readYamlToMap(mappingFilePath);
 
-        return mapping.get("for");
+        this.test_translate(mapping, "");
+
+        return null;
     }
 
 
     /**
      * Read YAML file and deserialize to Map<String, String>
-     * 
+     *
      * @param filepath YAML file path
      */
     private Map<String, String> readYamlToMap(String filepath) throws IOException, FileNotFoundException {
@@ -64,5 +63,59 @@ public class MapperService {
         }
 
         return map;
+    }
+
+    /** NOT COMPLETED. BUT I JUST WANT TO SLEEP
+     * Translate the native language code into english
+     * @param map Keyword mapping
+     * @param source_code native language code file path
+     * */
+    private String translate(Map<String, String> map, String source_code){
+
+        File sourceCode = new File(source_code);
+        String oldContent = "";
+        BufferedReader bufferedReader = null;
+        FileWriter fileWriter = null;
+
+        try{
+            bufferedReader = new BufferedReader(new FileReader(sourceCode));
+            String line = bufferedReader.readLine();
+
+            while (line != null)
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+
+                line = bufferedReader.readLine();
+            }
+
+            String newContent = "";
+
+            for(String key: map.keySet()){
+                newContent = oldContent.replaceAll(key, map.get(key));
+            }
+
+            String fileNameWithOutExt = FilenameUtils.removeExtension(source_code);
+
+            fileWriter = new FileWriter(new File(source_code + "target.txt"));
+
+            fileWriter.write(newContent);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void test_translate(Map<String, String> map, String source_code){
+        String code = "this is a සරල  code to test පරිවර්තනය";
+        String oldCode = code;
+
+        for(String key: map.keySet()){
+            code = code.replaceAll(map.get(key), key);
+        }
+
+        System.out.println(oldCode);
+        System.out.println(code);
     }
 }
